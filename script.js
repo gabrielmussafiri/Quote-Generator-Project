861db7e1c3a6c0d2c68800cf0fc7086721f7bdbc
gabrielmussafiri <musgabriel2@gmail.com>
2023-04-26 00:33:05 +0200

    button update

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const tnewQuoteBtn = document.getElementById("new-quote");
 
+let apiQuotes = [];
 // Show new Quote Function
 newQuote = () => {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
-};
 
+  // check if Authr field is blank and replace it with 'Unknow'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quotwe Length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+};
 // Get Quote From API
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
@@ -19,5 +36,10 @@ async function getQuotes() {
   }
 }
 
+// Tweet Quote
+tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+};
 // On Load
 getQuotes();
